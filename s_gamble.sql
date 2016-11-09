c6d0d3d1ef8b870ab62e78d3191d1150a47e8b16
LAPTOP-GNV1OT6P\Richi <richinel_jarbandhan@hotmail.com>
2016-11-09 15:50:11 +0100

    Added database file

--- s_gamble.sql
+++ s_gamble.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Machine: 127.0.0.1
--- Gegenereerd op: 03 nov 2016 om 19:22
--- Serverversie: 5.6.17
--- PHP-versie: 5.5.12
+-- Host: 127.0.0.1
+-- Gegenereerd op: 09 nov 2016 om 15:48
+-- Serverversie: 10.1.16-MariaDB
+-- PHP-versie: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,10 +14,10 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Databank: `s_gamble`
+-- Database: `s_gamble`
 --
 
 -- --------------------------------------------------------
@@ -26,25 +26,25 @@ SET time_zone = "+00:00";
 -- Tabelstructuur voor tabel `matches`
 --
 
-CREATE TABLE IF NOT EXISTS `matches` (
-  `match_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `matches` (
+  `match_id` int(11) NOT NULL,
   `home_squad` varchar(255) NOT NULL,
+  `home_score` int(11) NOT NULL,
   `home_notation` varchar(20) NOT NULL,
   `away_squad` varchar(255) NOT NULL,
+  `away_score` int(11) NOT NULL,
   `away_notation` varchar(20) NOT NULL,
-  `score` varchar(20) NOT NULL,
-  `date` varchar(20) NOT NULL,
-  PRIMARY KEY (`match_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `date` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `matches`
 --
 
-INSERT INTO `matches` (`match_id`, `home_squad`, `home_notation`, `away_squad`, `away_notation`, `score`, `date`) VALUES
-(1, 'Feyenoord', '2,50', 'Ajax', '2,30', '4 - 0', '23 - 10 - 2016'),
-(3, 'FC Test', '', 'Real Test', '', '3 - 4', ''),
-(4, 'FC Utrecht', '', 'AZ', '', '2 - 0', '');
+INSERT INTO `matches` (`match_id`, `home_squad`, `home_score`, `home_notation`, `away_squad`, `away_score`, `away_notation`, `date`) VALUES
+(1, 'Feyenoord', 4, '2,50', 'Ajax', 0, '2,30', '23 - 10 - 2016'),
+(3, 'FC Test', 5, '', 'Real Test', 4, '', ''),
+(4, 'FC Utrecht', 2, '', 'AZ', 3, '', '');
 
 -- --------------------------------------------------------
 
@@ -52,17 +52,16 @@ INSERT INTO `matches` (`match_id`, `home_squad`, `home_notation`, `away_squad`, 
 -- Tabelstructuur voor tabel `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `credits` int(11) NOT NULL,
-  `rights` int(10) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+  `rights` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `users`
@@ -70,9 +69,39 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `username`, `firstname`, `lastname`, `password`, `email`, `credits`, `rights`) VALUES
 (3, 'test', 'test', 'test', '098f6bcd4621d373cade4e832627b4f6', 'test', 25, 1),
-(4, 'Richinel', 'Richinel', 'Jarbandhan', 'fcea920f7412b5da7be0cf42b8c93759', 'rj_1993@live.nl', 100, 3),
-(5, 'GrappigeAnti', 'Jandino', 'Asporaat', 'fcea920f7412b5da7be0cf42b8c93759', 'j.asporaat@live.nl', 25, 1);
+(5, 'main_user', 'Main', 'User', '8aa0606b7e3547a234c80fc3017fae97', 'main_user@test.nl', 25, 1),
+(6, 'Richinel', 'Richinel', 'Jarbandhan', 'fcea920f7412b5da7be0cf42b8c93759', 'richinel_jarbandhan@hotmail.com', 25, 3);
 
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `matches`
+--
+ALTER TABLE `matches`
+  ADD PRIMARY KEY (`match_id`);
+
+--
+-- Indexen voor tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `matches`
+--
+ALTER TABLE `matches`
+  MODIFY `match_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT voor een tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
